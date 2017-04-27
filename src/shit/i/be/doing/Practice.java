930a419148ab93b89f5d5c9fff9894a51d387f34
Brendan Blencowe <Blenkz@Brendans-MacBook-Pro.local>
2017-04-26 22:40:02 -0400

    ef

---
+++ src/shit/i/be/doing/Practice.java
@@ -0,0 +1,12 @@
+package shit.i.be.doing;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Object o = "abc";
+		String s = "abc";
+		System.out.print(o.equals(s));
+	}
+
+}
---
+++ src/sht/she/be/doing/Question_2.java
@@ -0,0 +1,21 @@
+package sht.she.be.doing;
+
+//-----------------------------------------------------
+// Assignment 1
+// Written by: Harleen Tumber 27073488
+// For COMP 248 Section 2164 - Winter2017
+//------------------------------------------------------
+import java.util.Scanner;
+public class Question_2 {
+
+	public static void main(String[] args) {
+
+		int p = 9;
+		
+		System.out.print(++p + p++);
+
+		
+		
+	}
+
+}
